c1e6ce08f68200af309ab2fe183eb207d2c8b563
chocobone <yooho1030@naver.com>
2025-06-08 01:19:55 +0900

    http405 error solution 1

--- handler/vacuum_start.go
+++ handler/vacuum_start.go
@@ -15,6 +15,17 @@ type VacuumStart struct {
 }
 
 func (vs *VacuumStart) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Add CORS headers
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// Handle preflight OPTIONS request
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
