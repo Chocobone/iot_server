9bf5511326f574ec6ab8b6646c8a000a83af9ec9
chocobone <yooho1030@naver.com>
2025-06-09 00:16:48 +0900

    vacuum_start test18

--- config/secret.go
+++ config/secret.go
@@ -1,7 +1,7 @@
 package config
 
 func GetSecret() string {
-	return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkYjYwYWJkOTRlN2M0YTZjODkyMzQ3Y2JjOTgzZWUxYSIsImlhdCI6MTc0NzAyMTI5NCwiZXhwIjoyMDYyMzgxMjk0fQ.7mybkEqIh7coIRrVxkno8I1iTXCDz5wipB9rpomVUB0"
+	return "8242a6a157d70db99c3c97f92adab4d2"
 }
 
 func GetVacuumID() string {
